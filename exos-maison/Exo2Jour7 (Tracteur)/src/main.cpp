6d432bd8bc27400df831ed7368d81ae9841ccc80
jean-francois lidou <jean-francois.lidou@isi-mtl.com>
2021-08-13 09:07:23 -0400

    help exo

--- exos-maison/Exo2Jour7 (Tracteur)/src/main.cpp
+++ exos-maison/Exo2Jour7 (Tracteur)/src/main.cpp
@@ -1,19 +1,13 @@
 #include <SDL2/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-
 void drawAxis();
-
 void drawCircle(int resolution, int radius, int translateX);
-
 void drawBody(float velocityTranslationX, float velocityTranslationY);
-
 void drawWheels(float velocityTranslationX, float velocityTranslationY, float velocityRotation);
-
 void drawShovel(float velocityTranslationX, float velocityTranslationY, float shovelRotationVelocity);
-
-void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTranslationY, float &velocityRotation, float &shovelRotationVelocity);
-
+void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTranslationY, float &velocityRotation,
+              float &shovelRotationVelocity);
 int main(int argc, char **args) {
     SDL_Window *win;
     int width = 800, height = 600;
@@ -28,26 +22,22 @@ int main(int argc, char **args) {
     //creation du context
     SDL_GLContext context = SDL_GL_CreateContext(win);
     SDL_GL_SetSwapInterval(0);
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-width / 2, width / 2, -height / 2, height / 2);
-
     float velocityRotation = 0;
     float velocityTranslationX = 0;
     float velocityTranslationY = 0;
     float shovelRotationVelocity = 0;
-
-
     const Uint8 *state = nullptr;
-
     SDL_Event event;
+    glMatrixMode(GL_MODELVIEW);
     while (isRunning) {
-        glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
         //Clean window
-        glClearColor(0.0f, 0.f, 0.f,1.f); //permet d'expliquer avec quelle couleur on va remplir la memoire des couleurs
+        glClearColor(0.0f, 0.f, 0.f,
+                     1.f); //permet d'expliquer avec quelle couleur on va remplir la memoire des couleurs
         glClear(GL_COLOR_BUFFER_BIT); //permet de dire que la memoire des couleurs est prête à être modifié
 
         //Handle events
@@ -55,15 +45,15 @@ int main(int argc, char **args) {
         if (event.type == SDL_QUIT) {
             isRunning = false;
         }
-
         state = SDL_GetKeyboardState(NULL);
-        getState(state, velocityTranslationX, velocityTranslationY, velocityRotation,  shovelRotationVelocity);
+        getState(state, velocityTranslationX, velocityTranslationY, velocityRotation, shovelRotationVelocity);
 
-        drawAxis();
+//        drawAxis();
+        glTranslatef(velocityTranslationX, velocityTranslationY, 0);
         drawBody(velocityTranslationX, velocityTranslationY);
         drawWheels(velocityTranslationX, velocityTranslationY, velocityRotation);
         drawShovel(velocityTranslationX, velocityTranslationY, shovelRotationVelocity);
-        drawAxis();
+//        drawAxis();
 
 
         //Update screen
@@ -78,8 +68,8 @@ int main(int argc, char **args) {
     SDL_Quit();
     return 0;
 }
-
-void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTranslationY, float &velocityRotation, float &shovelRotationVelocity) {
+void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTranslationY, float &velocityRotation,
+              float &shovelRotationVelocity) {
     if (state[SDL_SCANCODE_D]) {
         velocityTranslationX += 1;
         velocityRotation += .1;
@@ -87,7 +77,7 @@ void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTr
     if (state[SDL_SCANCODE_A]) {
         velocityTranslationX -= 1;
         //Create Axis for the shovel
-        velocityRotation -=.1;
+        velocityRotation -= .1;
     }
     if (state[SDL_SCANCODE_W]) {
         velocityTranslationY += 1;
@@ -104,15 +94,13 @@ void getState(const Uint8 *state, float &velocityTranslationX, float &velocityTr
     }
     if (state[SDL_SCANCODE_RIGHT]) {
         if (shovelRotationVelocity < -70) {
-            shovelRotationVelocity = - 70;
-        }  else {
+            shovelRotationVelocity = -70;
+        } else {
             shovelRotationVelocity -= 1;
         }
     }
 }
-
 void drawShovel(float velocityTranslationX, float velocityTranslationY, float shovelRotationVelocity) {
-    glPopMatrix();
     glPushMatrix();
 
     //Create Axis for the shovel
@@ -125,7 +113,6 @@ void drawShovel(float velocityTranslationX, float velocityTranslationY, float sh
     glVertex2f(0, 0);
     glVertex2f(0, 20);
     glEnd();
-
     glTranslatef(velocityTranslationX, velocityTranslationY, 0);
     glRotatef(shovelRotationVelocity, 0, 0, 1);
     glBegin(GL_LINE_STRIP);
@@ -141,40 +128,34 @@ void drawShovel(float velocityTranslationX, float velocityTranslationY, float sh
     glVertex2f(10, -10);
     glVertex2f(0, 0);
     glEnd();
-
-}
-
-void drawWheels(float velocityTranslationX, float velocityTranslationY, float velocityRotation) {
     glPopMatrix();
+}
+void drawWheels(float velocityTranslationX, float velocityTranslationY, float velocityRotation) {
     glPushMatrix();
+    glTranslatef(-100, 0, 0);
+
+    //glRotatef(velocityRotation,0,0,1);
 
 
-    glTranslatef(velocityTranslationX, velocityTranslationY, 0);
-   //glRotatef(velocityRotation,0,0,1);
-
-
-    glBegin(GL_LINE_LOOP);
     int resolution = 100;
     int radius = 50;
     int translateX = -100;
-    drawCircle(resolution, radius, translateX);
-    glEnd();
-
-
-
-    glTranslatef(velocityTranslationX, velocityTranslationY, 0);
-    //glRotatef(velocityRotation,0,0,1);
-    glBegin(GL_LINE_LOOP);
-    translateX = 75;
-    drawCircle(resolution, radius, translateX);
-
-    glEnd();
-}
-
-void drawBody(float velocityTranslationX, float velocityTranslationY) {
-    glPopMatrix();
     glPushMatrix();
-    glTranslatef(velocityTranslationX, velocityTranslationY, 0);
+    glRotatef(velocityRotation, 0, 0, 1);
+    drawCircle(resolution, radius, translateX);
+    glPopMatrix();
+    glTranslatef(+175, 0, 0);
+    drawAxis();
+    //glRotatef(velocityRotation,0,0,1);
+//    translateX = 75;
+    glPushMatrix();
+    glRotatef(velocityRotation, 0, 0, 1);
+    drawCircle(resolution, radius, translateX);
+    glPopMatrix();
+    glPopMatrix();
+}
+void drawBody(float velocityTranslationX, float velocityTranslationY) {
+    glPushMatrix();
     glBegin(GL_LINE_STRIP);
     glColor3ub(255, 200, 0);
     glVertex2f(-100, 100);
@@ -185,25 +166,25 @@ void drawBody(float velocityTranslationX, float velocityTranslationY) {
     glVertex2f(-100, 0);
     glVertex2f(-100, 100);
     glEnd();
+    glPopMatrix();
 }
-
 void drawCircle(int resolution, int radius, int translateX) {
+    glBegin(GL_LINE_LOOP);
     for (int i = 0; i < resolution; i++) {
         // 2M_PI                ----> Point #15
         // theta increment?     <---- Point #1
         float theta = 2.0f * M_PI * float(i) / float(resolution);//get the current angle
-        if (i%2 == 0)
+        if (i % 2 == 0)
             radius = 45;
         float x = radius * cosf(theta);//calculate the x component
         float y = radius * sinf(theta);//calculate the y component
-        glVertex2f(x + translateX, y);//output vertex
+        glVertex2f(x, y);//output vertex
         radius = 50;
     }
+    glEnd();
 }
-
 void drawAxis() {
     //glLoadIdentity();
-    glPopMatrix();
     glPushMatrix();
     glBegin(GL_LINES);
     glColor3f(.5, 0, 0);
@@ -213,5 +194,6 @@ void drawAxis() {
     glVertex2f(0, 0);
     glVertex2f(0, 20);
     glEnd();
+    glPopMatrix();
 }
                         
